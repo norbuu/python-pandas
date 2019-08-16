7d98effcc6a88f52989dc46c8caf0e512be3b1d9
norbuu <budny.n@gmail.com>
2019-08-16 23:06:36 +0200

    simple df

--- simple_df/amazon_01.py
+++ simple_df/amazon_01.py
@@ -12,12 +12,12 @@ df = pd.DataFrame(d_raw)
 
 df['Wolumen'] = df["Wolumen"].apply(lambda x: x.replace(" ","")).\
                               apply(lambda x: int(x))
-                              
+
 df['Obrót'] = df["Obrót"].apply(lambda x: x.replace(" ","")).\
-                          apply(lambda x: int(x))       
-                          
+                          apply(lambda x: int(x))
+
 df['Udział'] = df["Udział"].apply(lambda x: x.replace("%","")).\
-                            apply(lambda x: float(x)) 
+                            apply(lambda x: float(x))
 
 #%%
 df["Zmiana %"] = df["Zmiana %"].apply(lambda x: x.replace("(","")).\
@@ -25,12 +25,12 @@ df["Zmiana %"] = df["Zmiana %"].apply(lambda x: x.replace("(","")).\
                                 apply(lambda x: x.replace("%","")).\
                                 apply(lambda x: x.replace("%","")).\
                                 apply(lambda x: float(x))
-                                
+
                                 #%%
 df["Profil"] = df["Profil"].apply(lambda x: x[0:4])
 
-                                
+
 #%%
-k = df.plot(x ="Profil", y="Zmiana %", kind = "bar", color= "blue", 
+k = df.plot(x ="Profil", y="Zmiana %", kind = "bar", color= "blue",
             title= "Zmiana %", figsize =(5,5))
 k.set_xlabel("Nazwa spółki")
